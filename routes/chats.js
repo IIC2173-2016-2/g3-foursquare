c069951d6d36f3bee80124892b0137c49e307ef3
jmdelatorre <jm.delatorre93@gmail.com>
2016-11-16 15:15:30 -0300

    Arreglos vistas, falta conexion con la API

--- routes/chats.js
+++ routes/chats.js
@@ -6,94 +6,117 @@ var fs = require('fs');
 var http = require('http');
 var jwt = require('jsonwebtoken');
 
-function ensureAuthenticated(req, res, next){
+function ensureAuthenticated(req, res, next) {
 
-	jwt.verify(req.cookies['access-token'], process.env.JWT_SECRET, function(err, decoded) {
-	  if(decoded)
-	  {
-	  	req.user = decoded._doc;
-	  	return next();
-	  }
-	  else
-	  {
-	  	res.redirect(302, login);
-	  }
-	  if(err)
-	  {
-	  	console.log(err);
-	  }
-	});
+    jwt.verify(req.cookies['access-token'], process.env.JWT_SECRET, function(err, decoded) {
+        if (decoded) {
+            req.user = decoded._doc;
+            return next();
+        } else {
+            res.redirect(302, login);
+        }
+        if (err) {
+            console.log(err);
+        }
+    });
 }
 
 
-router.get('/chat_created/:id',function(req,res){
-  var id = req.params.id
-  res.setHeader('chat_id',id);
-  res.setHeader('CHAT_API_SECRET_KEY',process.env.CHAT_API_SECRET_KEY)
-  res.redirect('/api/v1/is_chat_created');
+router.get('/chat_created/:id', function(req, res) {
+    console.log("revisa que esta creado");
+    var id = req.params.id
+    res.setHeader('chat_id', id);
+    res.setHeader('CHAT_API_SECRET_KEY', process.env.CHAT_API_SECRET_KEY)
+    res.redirect('/api/v1/is_chat_created');
 });
-router.get('/create_chat/:id/:venue',ensureAuthenticated, function(req, res) {
-  // LOOK IN COOKIE THE TOKEN USERNAME
-  var id = req.params.id
-  var venue = req.params.venue
-  res.setHeader('chat_id',id);
-  res.setHeader('chat_name',venue);
-  res.setHeader('CHAT_API_SECRET_KEY',process.env.CHAT_API_SECRET_KEY)
-  res.redirect('/api/v1/create_chat');
+router.get('/create_chat/:id/:venue', ensureAuthenticated, function(req, res) {
+    var id = req.params.id
+    var venue = req.params.venue
+
+		/*
+		AGREGAR LA CONEXION A LA API DEL SEBA, g3-auth para poder registrar a un
+		usuario un chat para luego ver en que chats esta conectado
+		*/
+
+    // https = require("https");
+		// var options = {
+		//   host: 'www.google.com',
+		//   port: 80,
+		//   path: '/upload',
+		//   method: 'POST'
+		// };
+		//
+		// var req = http.request(options, function(res) {
+		//   console.log('STATUS: ' + res.statusCode);
+		//   console.log('HEADERS: ' + JSON.stringify(res.headers));
+		//   res.setEncoding('utf8');
+		//   res.on('data', function (chunk) {
+		//     console.log('BODY: ' + chunk);
+		//   });
+		// });
+
+    // LOOK IN COOKIE THE TOKEN USERNAME
+
+    res.setHeader('chat_id', id);
+    res.setHeader('chat_name', venue);
+    res.setHeader('CHAT_API_SECRET_KEY', process.env.CHAT_API_SECRET_KEY)
+    res.redirect('/api/v1/create_chat');
 });
 
-router.get('/join_chat/:id/:venue',ensureAuthenticated, function(req, res) {
-  var id = req.params.id
-  var venue = req.params.venue
-  res.setHeader('chat_id',id);
-  res.setHeader('user_id',req.user._id);
-  res.setHeader('username',req.user.username);
-  res.setHeader('CHAT_API_SECRET_KEY',process.env.CHAT_API_SECRET_KEY);
-  res.redirect('/api/v1/join_chat');
+router.get('/join_chat/:id/:venue', ensureAuthenticated, function(req, res) {
+
+	/*
+	AGREGAR LA CONEXION A LA API DEL SEBA, g3-auth para poder registrar a un
+	usuario un chat para luego ver en que chats esta conectado
+	*/
+	
+    var id = req.params.id
+    var venue = req.params.venue
+    res.setHeader('chat_id', id);
+    res.setHeader('user_id', req.user._id);
+    res.setHeader('username', req.user.username);
+    res.setHeader('CHAT_API_SECRET_KEY', process.env.CHAT_API_SECRET_KEY);
+    res.redirect('/api/v1/join_chat');
 });
 
-function show_venue(req, res)
-{
-	if(true)
-	{
-		get_venue(req.params.location_id, function(venue_name, photos){
-			console.log("asdkaspodaosdaopskdaskdoaskdok");
-			console.log(photos);
-			res.render('create-chat', {
-				username: req.user.username,
-				venue_name: venue_name,
-				photos: photos,
-				id: req.params.location_id,
-				key: process.env.CHAT_API_SECRET_KEY,
-				host: req.hostname
-			});
-		});
-	}
-	else{
+function show_venue(req, res) {
+    if (true) {
+        get_venue(req.params.location_id, function(venue_name, photos) {
+            console.log("asdkaspodaosdaopskdaskdoaskdok");
+            console.log(photos);
+            res.render('create-chat', {
+                username: req.user.username,
+                venue_name: venue_name,
+                photos: photos,
+                id: req.params.location_id,
+                key: process.env.CHAT_API_SECRET_KEY,
+                host: req.hostname
+            });
+        });
+    } else {
 
-	}
+    }
 }
 
-function get_venue(venue_id, callback)
-{
-	https = require("https");
-	var body = [];
+function get_venue(venue_id, callback) {
+    https = require("https");
+    var body = [];
     var options = {
-            host: 'api.foursquare.com',
-            path: `/v2/venues/${venue_id}?client_id=${process.env.CLIENT_ID}&client_secret=${process.env.CLIENT_SECRET}&v=20161101&m=foursquare`
+        host: 'api.foursquare.com',
+        path: `/v2/venues/${venue_id}?client_id=${process.env.CLIENT_ID}&client_secret=${process.env.CLIENT_SECRET}&v=20161101&m=foursquare`
     };
 
-    https.request(options, function(res){
-		res.on('data', function(chunk){
-			body.push(chunk);
-		});
-		res.on('end', function(){
-			body = Buffer.concat(body).toString();
-			response = JSON.parse(body);
-			venue = response['response']['venue'];
-			callback(venue['name'], venue['photos']);
-		});
-	}).end();
+    https.request(options, function(res) {
+        res.on('data', function(chunk) {
+            body.push(chunk);
+        });
+        res.on('end', function() {
+            body = Buffer.concat(body).toString();
+            response = JSON.parse(body);
+            venue = response['response']['venue'];
+            callback(venue['name'], venue['photos']);
+        });
+    }).end();
 }
 
 module.exports = router;
--- routes/index.js
+++ routes/index.js
@@ -10,154 +10,138 @@ var jwt = require('jsonwebtoken');
 
 var login = '/users/login';
 
-router.get('/', function(req, res){
-	res.redirect('/locations');
+router.get('/', function(req, res) {
+    res.redirect('/locations');
 })
 
-router.get('/locations', ensureAuthenticated, function(req, res){
-	//if(validate_token(req)){
-	res.render('index',
-		{
-			host: req.hostname
-		});	//}
-	//else{
-		//res.redirect(login_path);
-	//}
+router.get('/locations', ensureAuthenticated, function(req, res) {
+    //if(validate_token(req)){
+    res.render('index', {
+        host: req.hostname
+    }); //}
+    //else{
+    //res.redirect(login_path);
+    //}
 });
 
-router.get('/my_chats', ensureAuthenticated, function(req, res){
-		res.render('my_chats');
+router.get('/my_chats', ensureAuthenticated, function(req, res) {
+    res.render('my_chats');
 });
 
 router.get('/locations/:location_id', ensureAuthenticated, show_venue);
 
 router.get('/my_chats_list/:lat/:long', function(req, res) {
-  var lat = req.params.lat;
-  var long = req.params.long;
+    var lat = req.params.lat;
+    var long = req.params.long;
 
-  console.log(lat);
-  console.log(long);
-	foursquare_venues(lat, long, function(venues){
-      res.json(venues);
-	});
+    console.log(lat);
+    console.log(long);
+    foursquare_venues(lat, long, function(venues) {
+        res.json(venues);
+    });
 });
 
 router.get('/foursquare/:lat/:long', function(req, res) {
-  var lat = req.params.lat;
-  var long = req.params.long;
+    var lat = req.params.lat;
+    var long = req.params.long;
 
-  console.log(lat);
-  console.log(long);
-	foursquare_venues(lat, long, function(venues){
-      res.json(venues);
-	});
+    console.log(lat);
+    console.log(long);
+    foursquare_venues(lat, long, function(venues) {
+        res.json(venues);
+    });
 });
 
-function show_venue(req, res)
-{
-	if(true)
-	{
-		get_venue(req.params.location_id, function(venue_name, photos){
-			if (photos != undefined) { // Hay foto
-				res.render('create-chat', {
-					username: req.user.username,
-					prefix: photos['prefix'],
-					suffix: photos['suffix'],
-					width: photos['width'],
-					height: photos['height'],
-					venue_name: venue_name,
-					photos: photos,
-					id: req.params.location_id,
-					host: req.hostname
-				});
-			}
-			else {
-				res.render('create-chat', {
-					username: req.user.username,
-					venue_name: venue_name,
-					photos: photos,
-					id: req.params.location_id,
-					host: req.hostname
-				});
-			}
+function show_venue(req, res) {
+    get_venue(req.params.location_id, function(venue_name, photos) {
+        if (photos != undefined) { // Hay foto
+            res.render('create-chat', {
+                username: req.user.username,
+                prefix: photos['prefix'],
+                suffix: photos['suffix'],
+                width: photos['width'],
+                height: photos['height'],
+                venue_name: venue_name,
+                photos: photos,
+                id: req.params.location_id,
+                host: req.hostname
+            });
+        } else {
+            res.render('create-chat', {
+                username: req.user.username,
+                venue_name: venue_name,
+                photos: photos,
+                id: req.params.location_id,
+                host: req.hostname
+            });
+        }
 
-		});
-	}
-	else{
+    });
 
-	}
 }
 
-function get_venue(venue_id, callback)
-{
-	https = require("https");
-	var body = [];
+function get_venue(venue_id, callback) {
+    https = require("https");
+    var body = [];
     var options = {
-            host: 'api.foursquare.com',
-            path: `/v2/venues/${venue_id}?client_id=${process.env.CLIENT_ID}&client_secret=${process.env.CLIENT_SECRET}&v=20161101&m=foursquare`
+        host: 'api.foursquare.com',
+        path: `/v2/venues/${venue_id}?client_id=${process.env.CLIENT_ID}&client_secret=${process.env.CLIENT_SECRET}&v=20161101&m=foursquare`
     };
 
-    https.request(options, function(res){
-		res.on('data', function(chunk){
-			body.push(chunk);
-		});
-		res.on('end', function(){
-			body = Buffer.concat(body).toString();
-			response = JSON.parse(body);
-			venue = response['response']['venue'];
-			if (venue != undefined) {
-				callback(venue['name'], venue['bestPhoto']);
-			}
-		});
-	}).end();
+    https.request(options, function(res) {
+        res.on('data', function(chunk) {
+            body.push(chunk);
+        });
+        res.on('end', function() {
+            body = Buffer.concat(body).toString();
+            response = JSON.parse(body);
+            venue = response['response']['venue'];
+            if (venue != undefined) {
+                callback(venue['name'], venue['bestPhoto']);
+            }
+        });
+    }).end();
 }
 
-function foursquare_venues(lat, long, callback)
-{
-        console.log('entro al foursquare');
-        https = require("https");
-        var lat = lat;
-        var long = long;
-        var body = [];
-        var venues;
-        var options = {
-                host: 'api.foursquare.com',
-                path: `/v2/venues/search?client_id=${process.env.CLIENT_ID}&client_secret=${process.env.CLIENT_SECRET}&v=20161101&m=foursquare&ll=${lat},${long}`
-        };
+function foursquare_venues(lat, long, callback) {
+    https = require("https");
+    var lat = lat;
+    var long = long;
+    var body = [];
+    var venues;
+    var options = {
+        host: 'api.foursquare.com',
+        path: `/v2/venues/search?client_id=${process.env.CLIENT_ID}&client_secret=${process.env.CLIENT_SECRET}&v=20161101&m=foursquare&ll=${lat},${long}`
+    };
 
-        https.request(options, function(res){
-		res.on('data', function(chunk){
-			body.push(chunk);
-		});
-		res.on('end', function(){
-			body = Buffer.concat(body).toString();
-			venues = JSON.parse(body)['response']['venues'];
+    https.request(options, function(res) {
+        res.on('data', function(chunk) {
+            body.push(chunk);
+        });
+        res.on('end', function() {
+            body = Buffer.concat(body).toString();
+            venues = JSON.parse(body)['response']['venues'];
 
-			callback(venues);
-		});
-	}).end();
-  console.log("termina de buscar cosas en foursquare");
+            callback(venues);
+        });
+    }).end();
 }
 
-function validate_token(req){}
+function validate_token(req) {}
 
-function ensureAuthenticated(req, res, next){
+function ensureAuthenticated(req, res, next) {
 
-		jwt.verify(req.cookies['access-token'], process.env.JWT_SECRET, function(err, decoded) {
-	  if(decoded)
-	  {
-	  	req.user = decoded._doc;
-	  	return next();
-	  }
-	  else
-	  {
-	  	res.redirect(302, login);
-	  }
-	  if(err)
-	  {
-	  	console.log(err);
-	  }
-	});
+    jwt.verify(req.cookies['access-token'], process.env.JWT_SECRET, function(err, decoded) {
+        if (decoded) {
+            req.user = decoded._doc;
+            return next();
+        } else {
+            res.redirect(302, login);
+        }
+        if (err) {
+            console.log(err);
+        }
+    });
 }
 
 module.exports = router;
